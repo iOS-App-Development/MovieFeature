dc579f48c1a1427b02243f68913cebb9b415d4bc
Muzammil Peer <muzammilpeer987@gmail.com>
2022-02-22 00:01:24 +0400

    module code added

--- MovieFeatureApp-iOS/MovieFeatureApp-iOS/SceneDelegate.swift
+++ MovieFeatureApp-iOS/MovieFeatureApp-iOS/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MovieFeature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,6 +17,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+//        if let rootNavigationController = window?.rootViewController as? UINavigationController, let rootViewController = rootNavigationController.viewControllers.first as? HomeViewController {
+//            rootViewController.viewModel = HomeViewModel.init()
+//        }
+        if let windows = self.window {
+            let navigationController:UINavigationController = UINavigationController.init()
+            let moviesModule:MovieFeatureAPI = MovieFeature.init(navigatoinController: navigationController)
+            let movies:UIViewController = moviesModule.load()
+            windows.rootViewController = navigationController
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
--- Sources/MovieFeature/MovieFeature.swift
+++ Sources/MovieFeature/MovieFeature.swift
@@ -24,3 +24,14 @@ extension MovieFeature:MovieFeatureAPI
         self.navigationController.popViewController(animated: true)
     }
 }
+
+/// A type that contributes to the object graph.
+public struct Module {
+    fileprivate let name: String
+    fileprivate let resolve: () -> Any
+    
+    public init<T>(_ name: String? = nil, _ resolve: @escaping () -> T) {
+        self.name = name ?? String(describing: T.self)
+        self.resolve = resolve
+    }
+}
